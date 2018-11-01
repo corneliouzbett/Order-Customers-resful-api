96f90e0e860dbb71f32ca951c0443770a9ed28c4
corneliouzBett <kbett68@gmail.com>
2018-11-01 10:09:38 +0300

    added item controller get all items test passed

--- test/controllers/item.controller.spec.js
+++ test/controllers/item.controller.spec.js
@@ -7,7 +7,12 @@ describe("Item Controller", function () {
     it("should return list of items", function () {
         let req = {};
         let res = {send: sinon.spy()};
+
         ItemController.getAllItems(req, res);
+        expect(res.send.calledOnce).to.be.false;
+        expect(res.send.isSinonProxy).to.be.true;
+        expect(res.send.args).to.be.array;
+        expect(res.send.calledTwice).to.be.false;
         console.log(res.send);
     });
 });
