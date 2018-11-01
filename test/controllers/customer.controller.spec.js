32e7091d0d81d4bcb4a76204eedc71444e5f64a6
corneliouzBett <kbett68@gmail.com>
2018-11-01 10:31:45 +0300

    Added customer test to get all customers

--- test/controllers/customer.controller.spec.js
+++ test/controllers/customer.controller.spec.js
@@ -1,13 +1,16 @@
-const customerMock = require('../mockData/customerMockData');
+const chai = require('chai');
+const expect = chai.expect;
+const sinon = require("sinon");
+const customerController = require("../../controllers/customer.controller");
 
-/*
-describe('#getAllCustomers', function () {
-    it('it should return all customers', function (done) {
-        customerMock.customerMock.findAll.then(function (customers) {
-            customers.should.have.be.a('object');
-            customers.customer_name.should.equal('bett kip');
-            done();
-
-        }).catch(done);
-    });
-});*/
+describe('Customer Controller', () => {
+    it('should return list of customers', () => {
+        let req = {};
+        let res = {send: sinon.spy()};
+        customerController.getAllCustomers(req, res);
+        expect(res.send.calledOnce).to.be.false;
+        expect(res.send.isSinonProxy).to.be.true;
+        expect(res.send.args).to.be.array;
+        expect(res.send.calledTwice).to.be.false;
+    })
+});
