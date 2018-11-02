3df6b4470ff50059347360918980da711a7c6d7b
corneliouzBett <kbett68@gmail.com>
2018-11-02 10:32:22 +0300

    added crud query interface

--- repository/SequelizeInstance.js
+++ repository/SequelizeInstance.js
@@ -4,3 +4,18 @@ module.exports.selectQueryInterface = (selectQuery) => {
     return Sequelize.getSequelizeInstance.query(selectQuery,
         {type: Sequelize.getSequelizeInstance.QueryTypes.SELECT});
 };
+
+module.exports.insertQueryInterface = (insertQuery) => {
+    return Sequelize.getSequelizeInstance.query(insertQuery,
+        {type: Sequelize.getSequelizeInstance.QueryTypes.INSERT} );
+}
+
+module.exports.updateQueryInterface = (updateQuery) => {
+    return Sequelize.getSequelizeInstance.query(updateQuery,
+        {type: Sequelize.getSequelizeInstance.QueryTypes.UPDATE} );
+}
+
+module.exports.deleteQueryInterface = (deleteQuery) => {
+    return Sequelize.getSequelizeInstance.query(deleteQuery,
+        {type: Sequelize.getSequelizeInstance.QueryTypes.DELETE} );
+}
