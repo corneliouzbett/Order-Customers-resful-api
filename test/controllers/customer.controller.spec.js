b2d559bdbb38e97eef09045a927c9de42640dba7
corneliouzBett <kbett68@gmail.com>
2018-11-06 10:03:25 +0300

    added controller test nd require dependencies.

--- test/controllers/customer.controller.spec.js
+++ test/controllers/customer.controller.spec.js
@@ -1,16 +1,39 @@
-const chai = require('chai');
-const expect = chai.expect;
-const sinon = require("sinon");
-const customerController = require("../../controllers/customer.controller");
+process.env.NODE_ENV = 'test';
 
-describe('Customer Controller', () => {
-    it('should return list of customers', () => {
-        let req = {};
-        let res = {send: sinon.spy()};
-        customerController.getAllCustomers(req, res);
-        expect(res.send.calledOnce).to.be.false;
-        expect(res.send.isSinonProxy).to.be.true;
-        expect(res.send.args).to.be.array;
-        expect(res.send.calledTwice).to.be.false;
-    })
+const sinon = require('sinon');
+const request = require('request');
+const chai = require('chai');
+const should = chai.should();
+const customerMock = require('../mockData/customer');
+const config = require('../../config/default.json');
+const base = `http://localhost:${config.port}`
+
+describe('Customers controller', () => {
+
+    beforeEach(() => {
+        const responseBody = customerMock.responseBody;
+        const responseObject = customerMock.responseObject;
+        this.get = sinon.stub(request, 'get');
+    });
+    afterEach(() => {
+        request.get.restore();
+    });
+
+    describe('GET /api/customers', () => {
+        it('it should return a list of customers', (done) => {
+            this.get.yields(null, responseObject, JSON.stringify(responseBody));
+            request.get(`${base}/api/customers`, (err, res, body) => {
+                res.statusCode.should.eql(200);
+                res.headers['content-type'].should.contain('application/json');
+                body = JSON.parse(body);
+                body.status.should.eql('success');
+                body.data.length.should.eql(3);
+                body.data[0].should.include.keys(
+                  'id', 'customer_name', 'phone_number'
+                );
+                body.data[0].name.should.eql('James woodward');
+                done();
+            });
+        });
+    });
 });
