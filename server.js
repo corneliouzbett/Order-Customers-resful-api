341ffc9d630344085defbbf62802f34cbf45a4f2
corneliouzbett <kbett68@gmail.com>
2018-11-08 14:06:36 +0300

    Files changes

--- server.js
+++ server.js
@@ -12,48 +12,12 @@ app.use(bodyParser.json({ type: 'application/json'}));
 
 require('./routes/routes.js')(app);
 
-const onError = (error) => {
-    if (error.syscall !== 'listen') throw error;
-
-    const porty = app.address;
-
-    const bind =
-        typeof porty === 'string'
-            ? `Pipe ${porty}`
-            : `Port + ${porty}`;
-
-    switch (error.code) {
-        case 'EACCES':
-            debug(`${bind} requires elevated privileges`);
-            process.exit(1);
-            break;
-        case 'EADDRINUSE':
-            debug(`${bind} is already in use`);
-            process.exit(1);
-            break;
-        default:
-            throw error;
-    }
-};
-
-const onListening = () => {
-    const addr = app.address();
-    const bind =
-        typeof addr === 'string'
-            ? `Pipe ${addr}`
-            : `Port ${addr.port}`;
-
-    debug(`Listening on ${bind}`);
-};
-
 models.sequelize.sync()
     .then(() => {
         app.listen(config.port, () => {
             debug('Express server listening on port', config.port);
             console.log('Customer-Orders server listening on port', config.port);
         });
-        app.on('error', onError);
-        app.on('listening', onListening);
     })
     .catch((err) => { throw err; });
 
